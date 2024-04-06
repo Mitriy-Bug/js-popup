c19a01a373fcc67881a9547fb6a056a6e29cc023
Mitriy_Bug <140303168+Mitriy-Bug@users.noreply.github.com>
2024-04-06 09:08:03 +0300

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,22 +1,24 @@
-const openPopupButtons = document.querySelectorAll('.open-popup');
-const popupBg = document.querySelectorAll('.popup__bg');
+onload = (event) => {
+    const openPopupButtons = document.querySelectorAll('.open-popup');
+    const popupBg = document.querySelectorAll('.popup__bg');
 
-openPopupButtons.forEach((button) => {
-    button.addEventListener('click', (e) => {
-        e.preventDefault();
-        Array.from(popupBg, (popup)=>{ 
-            if (popup.classList.contains(button.dataset.popup)) {
-                let closePopupButton = popup.querySelector('.close-popup');
-                popup.classList.add('active');
-                closePopupButton.addEventListener('click',() => {
-                    popup.classList.remove('active');
-                });
-                document.addEventListener('click', (e) => {
-                    if(e.target === popup) {
+    openPopupButtons.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            e.preventDefault();
+            Array.from(popupBg, (popup)=>{ 
+                if (popup.classList.contains(button.dataset.popup)) {
+                    let closePopupButton = popup.querySelector('.close-popup');
+                    popup.classList.add('active');
+                    closePopupButton.addEventListener('click',() => {
                         popup.classList.remove('active');
-                    }
-                });
-            } 
+                    });
+                    document.addEventListener('click', (e) => {
+                        if(e.target === popup) {
+                            popup.classList.remove('active');
+                        }
+                    });
+                } 
+            })
         })
-    })
-});
+    });
+};
