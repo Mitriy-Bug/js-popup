4fdf0cf96403fa9261f4988bf09d0fccbfbba051
Mitriy_Bug <140303168+Mitriy-Bug@users.noreply.github.com>
2024-04-06 08:54:48 +0300

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,22 @@
+const openPopupButtons = document.querySelectorAll('.open-popup');
+const popupBg = document.querySelectorAll('.popup__bg');
+
+openPopupButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        Array.from(popupBg, (popup)=>{ 
+            if (popup.classList.contains(button.dataset.popup)) {
+                let closePopupButton = popup.querySelector('.close-popup');
+                popup.classList.add('active');
+                closePopupButton.addEventListener('click',() => {
+                    popup.classList.remove('active');
+                });
+                document.addEventListener('click', (e) => {
+                    if(e.target === popup) {
+                        popup.classList.remove('active');
+                    }
+                });
+            } 
+        })
+    })
+});
